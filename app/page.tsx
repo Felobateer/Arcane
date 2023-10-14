053c4e0adcbb5507831f1d04d304bc5064a99205
Felobateer <felobateersamy01@gmail.com>
2023-10-14 23:21:30 +0200

    update phase 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Navbar from "./Components/Navbar";
 import Blog from "./Components/blog";
-import Friends from "./Components/friendsList";
+import FriendsList from "./Components/friendsList";
 import Post from "./Components/post";
-import { Box } from "@mui/material";
+import { Grid } from "@mui/material";
 import postArray from "./server and data/Posts.json";
+import Footer from "./Components/footer";
 
 export default function Home() {
   const posts = postArray;
@@ -14,22 +15,27 @@ export default function Home() {
       <Navbar />
 
       <main>
-        <Box></Box>
-        <Post />
-        {posts.map((post) => (
-          <Blog
-            key={post.id}
-            title={post.first_name + post.last_name}
-            date={post.Posted}
-            content={post.post}
-            pic={post.photo}
-            profileImage={post.profileImage}
-          />
-        ))}
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Post />
+            {posts.map((post) => (
+              <Blog
+                key={post.id}
+                title={post.first_name + post.last_name}
+                date={post.Posted}
+                content={post.post}
+                pic={post.photo}
+                profileImage={post.profileImage}
+              />
+            ))}
+          </Grid>
+          <Grid item xs={4}>
+            <FriendsList friends={posts} />
+          </Grid>
+        </Grid>
       </main>
-      <section>
-        <Friends />
-      </section>
+
+      <Footer />
     </>
   );
 }
